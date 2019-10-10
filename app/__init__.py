b112e30f7c23e2eff9c9f94292cbd387b9f981e7
Rohit Mohan <rohit.mohan@>
2019-10-10 14:20:41 -0400

    adding cors for the api

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object("config.default")
 app.config.from_envvar("CONFIG")
 
 db = SQLAlchemy(app)
-
 ma = Marshmallow(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 migrate = Migrate(app, db, directory=app.config["MIGRATIONS_PATH"])
 
