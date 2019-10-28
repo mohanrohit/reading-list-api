19677074aaecef4d19c1c2f06a5feb42cfe0c208
Rohit Mohan <rohit.mohan@gmail.com>
2019-10-28 17:20:48 -0400

    adding an existing book to a user; deleting a book from a user

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,9 @@
+end = 0
+
 from .model import Model, db
 
 from .book import Book
+from .user_books import UserBook
 
 class User(Model):
     __tablename__ = "users"
@@ -12,6 +15,10 @@ class User(Model):
     is_active = db.Column(db.Boolean(), default=False, nullable=False)
     books = db.relationship("Book", secondary="user_books", lazy="dynamic")
 
+    def has_book(self, book):
+        return True if UserBook.find_one(user_id=self.id, book_id=book.id) else False
+    end
+
     def create_book(self, book_params):
         try:
             new_book = Book.new(book_params)
@@ -21,13 +28,27 @@ class User(Model):
         self.add_book(new_book)
 
         return new_book
+    end
 
     def add_book(self, book):
+        if self.has_book(book):
+            return book
+        end
+
         self.books.append(book)
 
         self.save()
 
         return book
+    end
+
+    def delete_book(self, book):
+        users_book = UserBook.find_one(user_id=self.id, book_id=book.id)
+        if users_book:
+            users_book.delete()
+        end
+    end
 
     def __repr__(self):
         return f"User {self.first_name} {self.last_name}, {self.email}"
+    end
--- app/views/users_view.py
+++ app/views/users_view.py
@@ -1,7 +1,7 @@
 end = 0
 
 from app import app
-from app.models import User
+from app.models import User, Book
 
 from flask import url_for
 from flask import request
@@ -11,7 +11,7 @@ from flask_classful import route
 
 from .view import View
 
-from app.schemas import user_schema, users_schema, book_schema
+from app.schemas import user_schema, users_schema, book_schema, books_schema
 
 class UsersView(View):
     def before_request(self, name, **kwargs):
@@ -77,19 +77,63 @@ class UsersView(View):
         return jsonify({}), 204
     end
 
-    @route("/<int:id>/books", methods=["POST"])
-    def add_book(self, id):
-        user = request.user
-
-        try:
-            new_book = user.create_book(request.json)
-        except Exception as e:
-            return self.render_error(400, e.args[0])
-        end
-
+    def _create_new_book_for_user(self, user, params):
+        new_book = user.create_book(params)
         new_book.save()
 
-        return jsonify(book_schema.dump(new_book)), 201
+        return new_book
+    end
+
+    def _add_existing_book_to_user(self, user, book):
+        user.add_book(book)
+    end
+
+    # GET /api/v1/users/<id>/books
+    @route("/<int:id>/books", methods=["GET"])
+    def get_books(self, id):
+        return jsonify({ "books": books_schema.dump(request.user.books) })
+
+    # POST /api/v1/users/<id>/books
+    @route("/<int:id>/books", methods=["POST"])
+    def add_book(self, id):
+        if "book_id" in request.json: 
+            book = Book.find(request.json["book_id"])
+
+            if not book:
+                return self.render_error(400, f"The book with id {request.json['book_id']} was not found")
+            end
+
+            self._add_existing_book_to_user(request.user, book)
+        else:
+            try:
+                book = self._create_new_book_for_user(request.user, request.json)
+            except Exception as e:
+                return self.render_error(400, e.args[0])
+            end
+        end
+
+        # return 201 for both adding a new book and adding an existing book
+        # because adding an existing book can be considered to be creating
+        # a *new* book FOR the user
+        return jsonify(book_schema.dump(book)), 201
+    end
+
+    # DELETE /api/v1/users/<id>/books/<book_id>
+    @route("/<int:id>/books/<int:book_id>", methods=["DELETE"])
+    def delete_book(self, id, book_id):
+        book = Book.find(book_id)
+
+        if not book:
+            return self.render_error(400, f"The book with id {request.json['book_id']} was not found")
+        end
+
+        if not request.user.has_book(book):
+            return self.render_error(400, f"{request.user.email} does not have {book.title} on their reading list")
+        end
+
+        request.user.delete_book(book)
+
+        return jsonify({}), 204
     end
 end
 
