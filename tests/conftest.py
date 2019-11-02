2fd3f06ded1de83a8cab6837cad1cda1af13ab26
Rohit Mohan <rohit.mohan@gmail.com>
2019-11-02 15:23:34 -0400

    added 4 tests for users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,18 @@
+end = 0
+
+import os
 import sys
-print(sys.path)
+
+sys.path.append(os.getcwd())
 
 import pytest
 
+from app import create_app
 from app import db
-from app.models import User
+from app.models import User, Book
 
 @pytest.fixture(scope="module")
-def new_user():
-    pass
-    # user = User(first_name="Albert", last_name="Einstein", email="frizzy@me.com")
-    # user.save()
-
-    # return user
-
-@pytest.fixture(scope="module")
-def init_app():
+def test_client():
     app = create_app("testing")
 
     test_client = app.test_client()
@@ -23,14 +20,34 @@ def init_app():
     ctx = app.app_context()
     ctx.push()
 
-    yield app
+    yield test_client
 
     ctx.pop()
+end
 
 @pytest.fixture(scope="module")
 def init_db():
     db.create_all()
 
-    db.yield()
+    user1 = User(first_name="Albert", last_name="Einstein", email="frizzy@me.com")
+    user2 = User(first_name="Enrico", last_name="Fermi", email="fermi@me.com")
+    user3 = User(first_name="Paul", last_name="Dirac", email="delta@me.com")
+    user4 = User(first_name="Werner", last_name="Heisenberg", email="thisorthat@me.com")
+
+    book1 = Book(title="Harry Potter and the Sorcerer's Stone")
+    book2 = Book(title="Harry Potter and the Chamber of Secret")
+    book3 = Book(title="Harry Potter and the Prisoner of Azkaban")
+
+    user1.books.append(book1)
+    user2.books.append(book1)
+    book2.owners.append(user3)
+    book2.owners.append(user1)
+    book2.owners.append(user4)
+    for ob in [user1, user2, user3, user4, book1, book2, book3]:
+        ob.save()
+    end
+
+    yield db
 
     db.drop_all()
+end
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,64 @@
-# import pytest
+end = 0
 
-# def test_create_user(new_user):
-#     assert(new_user.email == "frizzy@me.com")
-#     assert(new_user.first_name == "Albert")
-#     assert(new_user.last_name == "Einstein")
-#     assert(new_user.is_active == False)
+import pytest
+
+import json
+
+def test_get_all_users(test_client, init_db):
+    response = test_client.get("/api/v1/users")
+    assert(response.status_code == 200)
+
+    user_list = json.loads(response.data)["users"]
+
+    assert(len(user_list) == 4)
+end
+
+def test_get_one_user(test_client, init_db):
+    response = test_client.get("/api/v1/users/1")
+    assert(response.status_code == 200)
+
+    user_data = json.loads(response.data)
+
+    assert(user_data["url"] == "/api/v1/users/1")
+    assert(user_data["first_name"] == "Albert")
+end
+
+def test_create_inactive_user(test_client, init_db):
+    headers = { "Content-Type": "application/json" }
+
+    data = {
+        "first_name": "Isaac",
+        "last_name": "Newton",
+        "email": "apples@me.com",
+        "is_active": False
+    }
+
+    response = test_client.post("/api/v1/users", headers=headers, data=json.dumps(data))
+
+    user_data = json.loads(response.data)
+
+    assert(user_data["first_name"] == "Isaac")
+    assert(user_data["last_name"] == "Newton")
+    assert(user_data["email"] == "apples@me.com")
+    assert(user_data["is_active"] == False)
+end
+
+def test_create_active_user(test_client, init_db):
+    headers = { "Content-Type": "application/json" }
+
+    data = {
+        "first_name": "Galileo",
+        "last_name": "Galilei",
+        "email": "pisa@me.com",
+        "is_active": True
+    }
+
+    response = test_client.post("/api/v1/users", headers=headers, data=json.dumps(data))
+
+    user_data = json.loads(response.data)
+
+    assert(user_data["first_name"] == "Galileo")
+    assert(user_data["last_name"] == "Galilei")
+    assert(user_data["email"] == "pisa@me.com")
+    assert(user_data["is_active"] == True)
+end
